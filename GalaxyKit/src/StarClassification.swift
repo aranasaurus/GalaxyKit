1c7297e68e14eec00d28c4631c8d24f4fbf83a77
Ryan Arana <ryan.arana@gmail.com>
2017-06-07 09:34:50 -0700

    The classification enum cases should still be capitalized, sorry Swift 3 styleguide....

--- GalaxyKit/src/StarClassification.swift
+++ GalaxyKit/src/StarClassification.swift
@@ -13,29 +13,29 @@ public extension Star {
      The classification of a star. Based primarily on [this wikipedia page](https://en.wikipedia.org/wiki/Stellar_classification#Harvard_spectral_classification).
      */
     public enum Classification: Int {
-        case o
-        case b
-        case a
-        case f
-        case g
-        case k
-        case m
+        case O
+        case B
+        case A
+        case F
+        case G
+        case K
+        case M
         
         var tempRange: (min: Kelvin, max: Kelvin) {
             switch self {
-            case .o:
+            case .O:
                 return (min: 30000, max: 52000)
-            case .b:
+            case .B:
                 return (min: 10000, max: 30000)
-            case .a:
+            case .A:
                 return (min: 7500, max: 10000)
-            case .f:
+            case .F:
                 return (min: 6000, max: 7500)
-            case .g:
+            case .G:
                 return (min: 5200, max: 6000)
-            case .k:
+            case .K:
                 return (min: 3700, max: 5200)
-            case .m:
+            case .M:
                 return (min: 2400, max: 3700)
             }
         }
@@ -46,19 +46,19 @@ public extension Star {
         
         var massRange: (min: SolarMass, max: SolarMass) {
             switch self {
-            case .o:
+            case .O:
                 return (min: 16, max: 90)
-            case .b:
+            case .B:
                 return (min: 2.1, max: 16)
-            case .a:
+            case .A:
                 return (min: 1.4, max: 2.1)
-            case .f:
+            case .F:
                 return (min: 1.04, max: 1.4)
-            case .g:
+            case .G:
                 return (min: 0.8, max: 1.04)
-            case .k:
+            case .K:
                 return (min: 0.45, max: 0.8)
-            case .m:
+            case .M:
                 return (min: 0.08, max: 0.45)
             }
         }
@@ -69,19 +69,19 @@ public extension Star {
         
         var radiusRange: (min: SolarRadius, max: SolarRadius) {
             switch self {
-            case .o:
+            case .O:
                 return (min: 6.6, max: 76)
-            case .b:
+            case .B:
                 return (min: 1.8, max: 6.6)
-            case .a:
+            case .A:
                 return (min: 1.4, max: 1.8)
-            case .f:
+            case .F:
                 return (min: 1.15, max: 1.4)
-            case .g:
+            case .G:
                 return (min: 0.96, max: 1.15)
-            case .k:
+            case .K:
                 return (min: 0.7, max: 0.96)
-            case .m:
+            case .M:
                 return (min: 0.33, max: 0.7)
             }
         }
@@ -92,19 +92,19 @@ public extension Star {
         
         var luminosityRange: (min: SolarLuminosity, max: SolarLuminosity) {
             switch self {
-            case .o:
+            case .O:
                 return (min: 30000, max: 1000000)
-            case .b:
+            case .B:
                 return (min: 25, max: 30000)
-            case .a:
+            case .A:
                 return (min: 5, max: 25)
-            case .f:
+            case .F:
                 return (min: 1.5, max: 5)
-            case .g:
+            case .G:
                 return (min: 0.6, max: 1.5)
-            case .k:
+            case .K:
                 return (min: 0.08, max: 0.6)
-            case .m:
+            case .M:
                 return (min: 0.0, max: 0.08)
             }
         }
@@ -120,19 +120,19 @@ public extension Star {
         
         var referenceColor: (hue: CGFloat, saturation: CGFloat) {
             switch self {
-            case .o:
+            case .O:
                 return (hue: 0.639, saturation: 0.457)
-            case .b:
+            case .B:
                 return (hue: 0.634, saturation: 0.398)
-            case .a:
+            case .A:
                 return (hue: 0.628, saturation: 0.251)
-            case .f:
+            case .F:
                 return (hue: 0.741, saturation: 0.035)
-            case .g:
+            case .G:
                 return (hue: 0.089, saturation: 0.11)
-            case .k:
+            case .K:
                 return (hue: 0.092, saturation: 0.35)
-            case .m:
+            case .M:
                 return (hue: 0.086, saturation: 0.636)
             }
         }
@@ -145,19 +145,19 @@ public extension Star {
         init(percentile: Double) {
             switch percentile {
             case percentile where percentile >= 0.9999997:
-                self = .o
+                self = .O
             case percentile where percentile >= 0.9987:
-                self = .b
+                self = .B
             case percentile where percentile >= 0.994:
-                self = .a
+                self = .A
             case percentile where percentile >= 0.97:
-                self = .f
+                self = .F
             case percentile where percentile >= 0.924:
-                self = .g
+                self = .G
             case percentile where percentile >= 0.879:
-                self = .k
+                self = .K
             default:
-                self = .m
+                self = .M
             }
         }
     }
