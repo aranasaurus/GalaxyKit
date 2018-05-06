117bf59da9cca51403227ccd18d590a660450b16
Ryan Arana <ryan.arana@gmail.com>
2018-05-06 10:02:24 -0700

    Add a test target and a very basic test to go with it

---
+++ GalaxyKitTests/GalaxyKitTests.swift
@@ -0,0 +1,22 @@
+//
+//  GalaxyKitTests.swift
+//  GalaxyKitTests
+//
+//  Created by Ryan Arana on 5/6/18.
+//  Copyright © 2018 Aranasaurus. All rights reserved.
+//
+
+import XCTest
+
+@testable import GalaxyKit
+
+class GalaxyKitTests: XCTestCase {
+    func testGalaxyDensityMap() {
+        let galaxy = Galaxy(densityMap: [1,2,3], continueGeneratingBeyondMap: false)
+        XCTAssertEqual(galaxy.sector(at: 0, 0)?.stars.count, 1)
+        XCTAssertEqual(galaxy.sector(at: 0, 1)?.stars.count, 2)
+        XCTAssertEqual(galaxy.sector(at: 0, 2)?.stars.count, 3)
+
+        XCTAssertNil(galaxy.sector(at: 0, 3))
+    }
+}
